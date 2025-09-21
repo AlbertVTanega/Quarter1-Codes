eabba8e3383af0a06b2c101a174a6c08d607d15d
AlbertVTanega <albertangel.tanega@smc.pshs.edu.ph>
2025-09-21 18:50:16 +0800

    code 1
    it asks for 3 numbers and add them all together and show the result in 2 sig figs

---
+++ code_1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Compute sum
+sum = num1 + num2 + num3
+
+# Display result with 2 decimal places
+print(f"\nThe total of the three numbers is: {sum:.2f}")
